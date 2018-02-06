cdaede553a86e3c68ecbf0a5de9f676315bb0f9d
Tchoko Apps <tchokoapps@gmail.com>
2018-02-06 20:12:59 +0100

    first commit

---
+++ src/main/java/com/tchoko/springboot/kafka/learnspringbootforkafka/LearnSpringbootForKafkaApplication.java
@@ -0,0 +1,12 @@
+package com.tchoko.springboot.kafka.learnspringbootforkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringbootForKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringbootForKafkaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tchoko/springboot/kafka/learnspringbootforkafka/LearnSpringbootForKafkaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tchoko.springboot.kafka.learnspringbootforkafka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LearnSpringbootForKafkaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
